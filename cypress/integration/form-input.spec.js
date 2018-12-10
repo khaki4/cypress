19fed2aea18b4e70bfd4a0de3eb62d2150e3ccb7
pi3rrot <4dakto@gmail.com>
2018-12-10 21:26:54 +0900

    Reduce Code Duplication in Cypress Tests with beforeEach

--- cypress/integration/form-input.spec.js
+++ cypress/integration/form-input.spec.js
@@ -1,13 +1,14 @@
 describe('Form Input', () => {
-  it('Focuses the input on load', () => {
+  beforeEach(() => {
     cy.visit('/');
+  });
+  it('Focuses the input on load', () => {
     cy.focused()
       .should('have.class', 'new-todo')
   });
 
-  it.only('should Accepts input',() => {
+  it('should Accepts input',() => {
     const typedText = 'New-Todo';
-    cy.visit('/');
     cy.get('.new-todo')
       .type(typedText)
       .should('have.value', typedText);
