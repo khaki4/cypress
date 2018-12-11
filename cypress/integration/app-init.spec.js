78a330578936807cd2794f2344b4c60c227c9c44
pi3rrot <4dakto@gmail.com>
2018-12-11 21:01:03 +0900

    Wait for XHR Responses

--- cypress/integration/app-init.spec.js
+++ cypress/integration/app-init.spec.js
@@ -1,8 +1,13 @@
 describe('App initialization', () => {
   it('Displays todos from API on load', () => {
     cy.server();
-    cy.route('GET', '/api/todos', 'fixture:todos');
+    cy.route('GET', '/api/todos', 'fixture:todos')
+      .as('load');
+
     cy.visit('/');
+
+    cy.wait('@load');
+
     cy.get('.todo-list li').should('have.length', 4)
   });
 });
